61a0be7172e9cb1046feaee13b7b0ffaf9b6f2c0
Andrew Koenig-Bautista <askoenig94@gmail.com>
2019-03-15 02:26:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@
 
 def countdown(number)
   while number >= 0
-  puts "#{number} SECOND(S)!"
-  number -= 1
+    puts "#{number} SECOND(S)!"
+      number -= 1
   end
     if number == 0 
       puts "HAPPY NEW YEAR!"
